98ba8e25f1128b71eb6de5d301c910a72ed2b611
IsabelGrimmig <63907706+IsabelGrimmig@users.noreply.github.com>
2022-05-10 21:49:07 +0200

    noder

--- main.c
+++ main.c
@@ -92,33 +92,161 @@ void addHeadKort(struct Bunke* Location){
 }
 
 void removeKort(struct Bunke* Location, struct Kort* specifikKort){
-    if (Location->head == Location->tail)
-
+    if (Location->head == NULL){                            //ingen kort i Bunken
+        return;
+    } else if (Location->head == Location->tail) {          //kun ét kort i bunken
+      Location->head = NULL;
+      Location->tail = NULL;
+    } else {                                                //Flere kort i bunken
+                                                            //Ret pointer til det kort der var located til det tidligere 'specifikKort'
+        if (specifikKort->prev != NULL) {                   //Kortet er ikke located til head
+            specifikKort->prev->next = specifikKort->next;
+        } else {}                                           //else gør ingen ting (kort located til head
+                                                            //Ret pointer til det kort der var located til det næste 'specifikKort'
+        if (specifikKort->next != NULL){                    //Kort located til tail
+            specifikKort->next->prev = specifikKort->prev;
+        }else {}                                            //else 'så ændres der ikke noget' kortet er located til tail
+    }
+    if (specifikKort->name != NULL){
+        free(specifikKort->name);
+    }
+    free(specifikKort);
 }
 
 void pushBunke(char *identifier){
-
+    struct Bunke* temp = malloc(sizeof(struct Bunke));
+    if (head_p == NULL){                                    //Der er ikke nogen bunker initialiseret
+        temp->prev = NULL;
+        head_p = temp;
+        tail_p =temp;
+    } else {                                                //der en eller flere bunker tilstede
+        tail_p->next = temp;
+        temp->prev = tail_p;
+        tail_p = temp;
+    }
+    temp->next = NULL;
+    strcpy(temp->beskrivelse, identifier);
 }
 
 void popBunke(struct Bunke* Location){
-
+    if (tail_p == NULL){                                    //der er overhoved ingen bunker(kant tilfælde)
+        return;
+    }
+    struct Bunke *temp = tail_p;
+    while (tail_p->head != NULL){
+        removeKort(tail_p,tail_p->tail);
+    }
+    if (head_p != tail_p){                                  //flere Bunker tilbage
+       tail_p = tail_p->prev;
+       tail_p->next = NULL;
+    } else {
+        head_p = NULL;
+        tail_p =NULL;
+    }
+    if (temp->beskrivelse !=NULL){
+        free(temp->beskrivelse);
+    }
+    free(temp);
 }
 
 void swap(struct Bunke* Location, int From, int To){
+    if (Location->head == NULL) {                            //kant tilfælde
+        return;
+    }
+        //vi har valgt at bruge array standarder til at bestemme location, dvs plads 0 er det første kort
+    struct Kort *temp = Location->head, *CFrom, *CTo = Location->head;
+    int index = 0, max = ((To - From) > 0) ? To : From;
+        if ((From - To) == 0){                              //der skal ikke gøres noget
+            return;
+        }
+        //Lokere begge kort
+        while (index <= max){
+            if (From == index){
+                CFrom = temp;
+            }
+            if (To == index){
+                CTo  = temp;
+            }
+            temp = temp->next;
+        }
 
-}
+        //Ombytte kortene, ved at bruge tamp og ændringen af pointer
+        //først ændres poninterne til kortet
+        temp = malloc(sizeof(struct Kort));
+        temp->prev = CFrom->prev;
+        temp->next =CFrom->next;
+        CFrom->prev = CTo->prev;
+        CFrom->next = CTo->next;
+        CTo->prev = temp->prev;
+        CTo->next = temp->next;
+
+        //Nu ændres pointerne på kortene i bunken for at kunne se de rigtige kort
+    if (CTo->prev !=NULL){ CTo->prev->next; }
+    if (CTo->next != NULL){ CTo->next->prev; }
+    if (CFrom->prev != NULL){ CFrom->prev->next; }
+    if (CFrom->next != NULL){ CFrom->next->prev = CFrom;}
+    //fjerner the temp node, der blev lavet
+        free(temp);
+    }
+
 
 void duplicateKort(struct Kort* duplicate, struct Bunke* placeWhere){
+    pushKort(placeWhere);
+    placeWhere->tail->number = duplicate->number;
+    placeWhere->tail->suit = duplicate->suit;
+    placeWhere->tail->erSkjult = duplicate->erSkjult;
+    strcpy(placeWhere->tail->name, duplicate->name);
+}
+
+void moveKort(struct Bunke* From, struct Bunke* To){
+    if(From->head == NULL){                              //kant tilfælde
+        return;
+    }
+    // Frakobling af kortet fra From bunken
+    struct Kort *temp = From->tail;
+    if(From->tail == From->head){                       // Kun et kort i bunken Form
+        From->head = NULL;
+        From->tail = NULL;
+    } else {                                            // flere kort i bunken
+        From->tail = From->tail->prev;
+        From->tail->next = NULL;
+    }
+    // Til slutningen af kortet til To bunken
+    if(To->tail == NULL){                               //ingen kort i bunken
+        To->head = temp;
+        To->tail = temp;
+        temp->prev = NULL;
+
+    } else{                                             //En eller flere kort i To bunken
+        To->tail->next = temp;
+        temp->prev = To->tail;
+        To->tail = temp;
+    }
 
 }
 
-void moveKort(struct Bunke* From, struct Bunke* to){
-
+void moveKortR(struct Bunke* From, struct Kort* whichCard, struct Bunke* To){
+    if(From->head == NULL){//edge case
+        return;
+    }
+    if(From->head == whichCard){                        //Flyt hele indholdet af bunken
+        From->head = NULL;
+        From->tail = NULL;
+    } else {                                            // Flyt dele af bunkens indhold
+        From->tail = whichCard->prev;
+        From->tail->next = NULL;
+    }
+    //tilføj kortet til en ny bunke
+    To->tail->next = whichCard;
+    whichCard->prev = To->tail;
+    while (whichCard->next != NULL){
+        whichCard = whichCard->next;
+    }
+    To->tail = whichCard;
 }
 
-void moveKortR(struct Bunke* From, struct Kort* whichCart, struct Bunke* To){
 
-}
+//Logik til spilstrukturen
 
 
 
